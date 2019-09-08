7c0358b03bb6dc9b5c96164f614d77d1af59e2e6
Alexander Makarov <magrus87@gmail.com>
2019-09-08 18:59:41 +0400

    start

---
+++ TinkoffPoints/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  TinkoffPoints
+//
+//  Created by Alexander Makarov on 08/09/2019.
+//  Copyright © 2019 magrus87. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
